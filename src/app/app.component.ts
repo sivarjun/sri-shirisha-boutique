0b26aa57fdd44145ad299fdab7234f6c4ee107d5
Chikkala <sivarjauna.chikkala@accenture.com>
2018-08-22 18:58:53 +0530

    bootstrap added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'boutique';
+export class AppComponent implements OnInit{
+  
+  title = 'Sri Shirisha Boutique';
+
+constructor(private http:HttpClient){
+
+}
+ngOnInit(): void {
+  this.http.get<any[]>('http://localhost:59226/api/Customer')
+  .subscribe(result=>console.log(result));
+}
 }
