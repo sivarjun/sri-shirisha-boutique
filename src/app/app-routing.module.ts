905b6b17bdbba9a5837b66bfa154ae01b539822e
Chikkala <sivarjauna.chikkala@accenture.com>
2018-08-23 17:09:27 +0530

    create customer form added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CustomerListComponent } from './customer/customer-list.component';
+import { EditCustomerComponent } from './customer/edit-customer/edit-customer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'',component:HomeComponent},
+  {path:'home', component:HomeComponent},
+  {path:'customer',component:CustomerListComponent},
+  {path:'customer/:id',component:EditCustomerComponent},
+  {path:'**',redirectTo:''}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CustomerListComponent } from './customer/customer-list.component';
+import { EditCustomerComponent } from './customer/edit-customer/edit-customer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CustomerListComponent,
+    EditCustomerComponent
+ 
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
